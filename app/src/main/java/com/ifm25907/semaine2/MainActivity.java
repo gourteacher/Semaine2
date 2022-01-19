e8cc34531f96860e48f27ae5c92f5e9b8cec3c61
gourteacher <ouaazs@algonquincollege.com>
2022-01-19 16:12:55 -0500

    + bundle

---
+++ app/src/main/java/com/ifm25907/semaine2/MainActivity.java
@@ -0,0 +1,80 @@
+package com.ifm25907.semaine2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG ="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i( TAG, "onCreate");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i( TAG, "onRestoreInstanceState"       );
+
+        final EditText editText = findViewById(R.id.editText);
+        CharSequence  userText = savedInstanceState.getCharSequence("saveText");
+        editText.setText(userText);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i( TAG, "onSaveInstanceState");
+
+        final EditText editText = findViewById(R.id.editText);
+        CharSequence userText = editText.getText();
+        outState.putCharSequence("saveText", userText);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i( TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i( TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i( TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i( TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i( TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i( TAG, "onResume");
+    }
+}
